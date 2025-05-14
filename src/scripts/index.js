bbaed1e91b0b235bdf06e872f805362814337a79
Sveta <lanatovmasyan@yandex.ru>
2025-05-14 18:06:53 +0300

    sprint7 beginning

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -59,10 +59,11 @@ const profileJob = document.querySelector(".profile__description");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
 
-//4. Редактирование имени и инф
+//4. Редактирование имени и инф (ред 7 спринт)
 profileEditButton.addEventListener("click", function () {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileJob.textContent;
+  toggleButton(profileEditForm, saveButton);
   openPopup(popupEdit);
 });
 
@@ -78,7 +79,7 @@ function handleFormSubmit(evt) {
   closePopup(popupEdit);
 }
 
-profileEditForm.addEventListener("submit", handleFormSubmit); // Теперь слушаем событие отправки формы
+profileEditForm.addEventListener("submit", handleFormSubmit);
 
 // 6. Добавление карточки
 const formNewCard = popupNewCard.querySelector("form");
@@ -124,4 +125,79 @@ function handleImageClick(link, name) {
   popupImageElement.alt = name;
   popupCaption.textContent = name;
   openPopup(popupImage);
+}
+
+//7 спринт
+
+const allForms = document.querySelectorAll(".popup__form");
+
+//const popupContent = document.querySelector(".popup__content");
+//const popupInputs = popupContent.querySelectorAll(".popup__input");
+
+allForms.forEach((form) => {
+  const inputs = form.querySelectorAll(".popup__input");
+  const button = form.querySelector(".popup__button");
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      checkInputValidity(input);
+      toggleButton(form, button);
+    });
+  });
+});
+
+const showPopupError = function (input, errorMessage) {
+  const errorElement = input.nextElementSibling;
+  input.classList.add("popup__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__error_visible");
+};
+
+const hidePopupError = function (input) {
+  const errorElement = input.nextElementSibling;
+  input.classList.remove("popup__input_type_error");
+  errorElement.textContent = "";
+  errorElement.classList.remove("popup__error_visible");
+};
+
+const checkInputValidity = function (input) {
+  if (!input.validity.valid) {
+    let errorMessage = "";
+
+    if (input.validity.valueMissing) {
+      errorMessage = "Вы пропустили это поле.";
+    } else if (input.validity.tooShort) {
+      const minLength = input.getAttribute("minlength");
+      const currentLength = input.value.length;
+      errorMessage = `Минимальное количество символов: ${minLength}. Длина текста сейчас: ${currentLength} символ${
+        currentLength > 1 ? "ов" : ""
+      }.`;
+    } else if (input.validity.typeMismatch && input.type === "url") {
+      errorMessage = "Введите адрес сайта.";
+    }
+
+    showPopupError(input, errorMessage);
+  } else {
+    hidePopupError(input);
+  }
+};
+
+// Проверка каждого поля (ред в 7 спринте)
+//popupInputs.forEach((input) => {
+//  input.addEventListener("input", function () {
+//    checkInputValidity(input);
+//    toggleButton(profileEditForm,saveButton)
+//  });
+//});
+
+//переключение стилей нопки "сохранить"
+
+const saveButton = popupEdit.querySelector(".popup__button");
+
+function toggleButton(form, button) {
+  const inputs = Array.from(form.querySelectorAll(".popup__input"));
+  const formValid = inputs.every(function (input) {
+    return input.validity.valid;
+  });
+  button.disabled = !formValid;
 }
